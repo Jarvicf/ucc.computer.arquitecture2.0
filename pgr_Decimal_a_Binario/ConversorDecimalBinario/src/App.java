dec08c00daab2f34990f1c9cfccd8a28b3ba51fc
Jarvicf <jarvicf@gmail.com>
2023-03-03 23:21:38 -0500

    ultima version

--- pgr_Decimal_a_Binario/ConversorDecimalBinario/src/App.java
+++ pgr_Decimal_a_Binario/ConversorDecimalBinario/src/App.java
@@ -5,62 +5,115 @@ public class App {
         Scanner obtener = new Scanner (System.in);
         int numref = 0;
         int menu = 0;
-        String Resultado  =  "";
-        String ResultadoGFinal = "";
-        while(menu == 0){
         System.out.println("Bienvenido al conversor de Números");
         System.out.println("############## MENU ###############");
-        System.out.println("Seleccione una opción");
-        System.out.println("1 > Conversión de Decimal a Binarios");
-        System.out.println("2 > Conversión de Binario a Decimal");
-        System.out.println("9 > Salir");
+        try{               
+            while(menu == 0){
+                System.out.println("Seleccione una opción");
+                System.out.println("📌 1 > Conversión de Decimal a Binarios");
+                System.out.println("📌 2 > Conversión de Binario a Decimal");
+                System.out.println("📌 9 > Salir");
+
+                System.out.print(">>");
+
+            
+                String Resultado  =  "";
+                String ResultadoGFinal = "";
+
+                //Capturamos la opcion de usuario
+                int opc = obtener.nextInt();
         
-        //Capturamos la opcion de usuario
-       int opc = obtener.nextInt();
-        //Ciclo
-        
-             //Menu
-        switch(opc){
-            case 1:
-            int divisor = 2;
-            int cociente = 0;
-            int residuo = 0;
+                //Menu
+                switch(opc){
+                    //##############################
+                    //Converción de Decimal a Binario
+                    case 1:
+                    int divisor = 2;
+                    int cociente = 0;
+                    int residuo = 0;
     
-            String operar = "S" ;
-            System.out.println("Conversión de Binario a Decimal");
-            System.out.print("Ingrese Numero a Convertir: ");
-            int numero = obtener.nextInt();
-            numref =  numero;
-                if(numero >=1){
-                    while(operar == "S"){
-                         //Operamos y obtenemos el Cociente y el residuo
-                         cociente = numero / divisor;
-                         residuo = numero % divisor;
-                         if (cociente >= 2)
-                            {
-                                Resultado = Resultado + residuo;                            
-                                numero = cociente;
-                            }
-                            else
-                            {
-                                Resultado = Resultado + residuo;                                //txtResultado.Text = Resultado;
-                                operar = "N";
-                            }
+                    String operar = "S" ;
+                    System.out.println("Conversión de Binario a Decimal");
+                    System.out.print("Ingrese Número a Convertir: ");
+                    int numero = obtener.nextInt();
+                    numref =  numero;
+                    if(numero >=2){
+
+                        while(operar == "S"){
+                            //Operamos y obtenemos el Cociente y el residuo
+                            cociente = numero / divisor;
+                            residuo = numero % divisor;
+                            if (cociente >= 2)
+                                {
+                                    Resultado = Resultado + residuo;                            
+                                    numero = cociente;
+                                }
+                                else
+                                {
+                                    Resultado = Resultado + residuo;                                
+                                    operar = "N";
+                                }
+                        }
+                        Resultado = Resultado + cociente;
+                        //Arreglamos el número
+                        for(int i = Resultado.length() -1;i>0 -1;i--)
+                        {
+                            char Digito = Resultado.charAt(i);
+                            //System.out.print(Digito);
+                            ResultadoGFinal = ResultadoGFinal + Digito;
+                        }
+                    }else{
+                        switch(numero){
+                        case 0:
+                            ResultadoGFinal = "0";
+                            break;
+                        case 1:
+                            ResultadoGFinal = "1";
+                            break;
+                        }
                     }
-                    Resultado = Resultado + cociente;
-                }
-                break;
-            }        
-            //Arreglamos el número
-            for(int i = Resultado.length() -1;i>0 -1;i--)
-            {
-                char Digito = Resultado.charAt(i);
-                //System.out.print(Digito);
-                ResultadoGFinal = ResultadoGFinal + Digito;
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    System.out.println("😀>> En número decimal "+ "["+numref+"]"+ " Corresponde a "+ "["+ResultadoGFinal+"]"+ " En número Binario 😀"); 
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    System.out.println("");
+                    break;
+
+                //################################    
+                //Converción de Binario a Decinal
+                case 2:
+                    System.out.println("Conversión de Binario a Decimal");
+                    System.out.print("Ingrese Número a Convertir: ");
+                    String Numero = obtener.next();
+                    int exp = 0;
+                    double nunDec = 0;
+                    for(int i = Numero.length() -1  ;i >= 0;i--)
+                    {
+                        char  dato = Numero.charAt(i);
+                        String Digito = String.valueOf(dato);
+                        if(Digito.equals("1"))
+                        {
+                            nunDec = nunDec + Math.pow(dato, exp);
+                            exp ++;
+                        }else
+                        {
+                            exp ++;
+                        }
+                    }
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    System.out.println(">> En número Binario "+ "["+Numero+"]"+ " Corresponde a "+ "["+nunDec+"]"+ " En número Decimal"); 
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    System.out.println("");
+
+                    break;    
+                //Salir
+                case 9:
+                    menu = 1;
+                    break;
+                }        
             }
-            System.out.println(">> En número decimal "+ numref + " Corresponde a "+ResultadoGFinal+ " En número Binario"); 
-            System.out.println("");
-        }
         obtener.close();
+        }catch (Exception ex){
+            System.out.println("Se ha presentado un error "+ ex);       
+        } 
     }
 }
