1ba5e9387b740961a60b4a2fffdf9f3a1aa5ed00
Jarvicf <jarvicf@gmail.com>
2023-04-24 17:15:30 -0500

    MultiConversor

--- pgr_Convertidor_Numeros/MultiConversor/src/App.java
+++ pgr_Convertidor_Numeros/MultiConversor/src/App.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 public class App {
  
     public static void main(String[] args) {
- 
+        //Coleres
+        String verde = "\033[32m";
+        String amarilla = "\033[103m";
+        String rojo = "\033[31m";
+        String reset = "\033[39m";
+        Conversores conversores = new Conversores();
         Scanner capturar = new Scanner(System.in);
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
         System.out.println("BIENVENIDO AL MULTICONVERSOR");
         while (!salir) {
  
-            System.out.println("1. Conversor Decimal a Binarios");
+            System.out.println("1. Conversor Decimal a Binario");
             System.out.println("2. Conversor Binario a Decimal");
             System.out.println("3. Conversor Decimal a Octal");
             System.out.println("4. Conversor Octal a Decimal");
             System.out.println("5. Conversor Decimal a Hexadecimal");
-            System.out.println("6. Conversor Hexadecimal a DEcimal");
+            System.out.println("6. Conversor Hexadecimal a Decimal");
             System.out.println("7. Salir");
  
             try {
@@ -27,26 +32,77 @@ public class App {
  
                 switch (opcion) {
                     case 1:
-                        System.out.println("Has seleccionado la opcion 1");
+                    System.out.println("--Conversor Decimal a Binario");
+                    System.out.println("--Ingresa un numero Decimal");
+                    System.out.print("-->");
+                    Long Dato = capturar.nextLong();
+                    //Resultado
+                    System.out.println("😀>> En número decimal "+ verde +"["+Dato+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.convertir_Decimal_Binario(Dato)+"]"+ reset+" En número Binario 😀"); 
+                    System.out.println();    
                         break;
                     case 2:
-                        System.out.println("Has seleccionado la opcion 2");
+                    System.out.println("--Conversor Binario a Decimal");
+                    System.out.println("--Ingresa un numero Binario");
+                    System.out.print("-->");
+                    String DatoC2 = capturar.next();
+                    //Resultado
+                    System.out.println("😀>> En número binario "+ verde +"["+DatoC2+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.convertir_Binario_Decimal(DatoC2)+"]"+ reset+" En número decimal 😀"); 
+                    System.out.println();    
                         break;
                     case 3:
-                        System.out.println("Has seleccionado la opcion 3");
+                    System.out.println("--Conversor Decimal a Octal");
+                    System.out.println("--Ingresa un numero Decimal");
+                    System.out.print("-->");
+                    int DatoC3 = capturar.nextInt();
+                     //Resultado
+                     System.out.println("😀>> En número decimal "+ verde +"["+DatoC3+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.convertir_decimal_Octal(DatoC3)+"]"+ reset+" En número octal 😀"); 
+                     System.out.println();    
                         break;
                     case 4:
+                    System.out.println("--Conversor Octal a Decimal");
+                    System.out.println("--Ingresa un numero Octal");
+                    System.out.print("-->");
+                    int DatoC4 = capturar.nextInt();
+                    if(conversores.Verificar_Numero_(DatoC4 ,0 ,7)){                     
+                    System.out.println("😀>> En número octal "+ verde +"["+DatoC4+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.convertir_octal_Decimal(String.valueOf(DatoC4))+"]"+ reset+" En número decimal 😀"); 
+                    }else{
+                    System.out.println(rojo + ">> ❌ Numero Ingresado no es Octal " + verde+"["+DatoC4+"]" + reset); 
+                    }
+                    System.out.println();
+                        break;
+                    case 5:
+                    System.out.println("--Decimal a Hexadecimal");
+                    System.out.println("--Ingresa un numero Decimal");
+                    System.out.print("-->");
+                    int DatoC5 = capturar.nextInt();
+                    System.out.println("😀>> En número decimal "+ verde +"["+DatoC5+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.Decimalhexadecimal(DatoC5) +"]"+ reset+" En número Hexadecimal 😀"); 
+                    
+                    System.out.println();
+                        break;
+                    case 6:
+                    System.out.println("--Conversor Hexadecimal a Decimal");
+                    System.out.println("--Ingresa un numero Hexadecimal");
+                    System.out.print("-->");
+                    String DatoC6 = capturar.next();
+                    if(conversores.checkNumber(DatoC6)){
+                        System.out.println("😀>> En número decimal "+ verde +"["+DatoC6+"]"+ reset+" Corresponde a "+ verde+"["+ conversores.convertir_Hexadecimal_Decimal(DatoC6)+"]"+ reset+" En número Hexadecimal 😀"); 
+                    }
+                    System.out.println();   
+                        break;
+                    case 7:
                         salir = true;
                         break;
                     default:
                         System.out.println("Solo números entre 1 y 4");
                 }
+                
             } catch (InputMismatchException e) {
                 System.out.println("Debes insertar un número");
                 capturar.next();
             }
+            
         }
- 
+        //capturar.close();
     }
  
 }
--- pgr_Convertidor_Numeros/MultiConversor/src/Conversores.java
+++ pgr_Convertidor_Numeros/MultiConversor/src/Conversores.java
@@ -1,3 +1,228 @@
 public class Conversores {
+
+    final String Hex[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+
+    //Metodo Binario
+    public String convertir_Decimal_Binario(long decimal) {
+        if (decimal <= 0) {
+            return "0";
+        }
+        StringBuilder binario = new StringBuilder();
+        while (decimal > 0) {
+            short residuo = (short) (decimal % 2);
+            decimal = decimal / 2;
+            // Insertar el dígito al inicio de la cadena
+            binario.insert(0, String.valueOf(residuo));
+        }
+        return binario.toString();
+    }
+
+    public double convertir_Binario_Decimal(String Numero){
+
+        int exp = 0;
+        double nunDec = 0;
+        for(int i = Numero.length() -1  ;i >= 0;i--){
+            char  dato = Numero.charAt(i);
+            String Digito = String.valueOf(dato);
+            if(Digito .equals("1"))
+            {
+                nunDec = nunDec + Math.pow(2, exp);
+                exp ++;
+            }else if(Digito .equals("0"))
+            {
+                exp ++;
+            }
+        }
+        return nunDec;
+    }
+        
+    //Metodo octal 
+     public long convertir_octal_Decimal(String octal) {
+        long decimal = 0;
+        int potencia = 0;
+        for (int x = octal.length() - 1; x >= 0; x--) {
+            int valorActual = Character.getNumericValue(octal.charAt(x));
+            long elevado = (long) Math.pow(8, potencia) * valorActual;
+            decimal += elevado;
+            potencia++;
+        }
+        return decimal;
+    }
+
+    public String convertir_decimal_Octal(int decimal) {
+        String octal = "";
+        String caracteresOctales = "01234567";
+        while (decimal > 0) {
+            int residuo = decimal % 8;
+            octal = (caracteresOctales.charAt(residuo) + octal);
+            decimal /= 8;
+        }
+        return octal;
+    }
     
+    public boolean Verificar_Numero_(int numeroInto, int inferior, int maximo){
+        
+          String numeroIn = Integer.toString(numeroInto);
+        
+        for(int i = numeroIn.length() -1  ;i >= 0;i--)
+        {
+            char  dato = numeroIn.charAt(i);
+            String Digito = String.valueOf(dato);
+            int datounico = Integer.parseInt(Digito);
+            if(datounico < inferior || datounico > maximo)
+            {
+                return false;
+            }                
+        }
+        
+        return true;         
+
+    }   
+
+    //Metodos Hexadecimal
+    public boolean checkNumber(String number) {
+        boolean checkhex = true;
+        for (int i = number.length()-1; i >= 0; i--){
+            char dato = number.charAt(i);
+            String digito = String.valueOf(dato);
+            if (!existeEnArreglo(Hex, digito )){
+                System.out.println("❌ El dato ingresado no es un numero Octal ❌");
+                checkhex = false;
+                break;
+            }
+        }
+        return checkhex;
+    }
+
+    private boolean existeEnArreglo(String[] arreglo, String busqueda) {
+        for (int x = 0; x < arreglo.length; x++) {
+            String dato = arreglo[x];
+          if (arreglo[x].equals(busqueda)) {
+            return true;
+          }
+        }
+        return false;
+    }
+
+    public long convertir_Hexadecimal_Decimal(String hexadecimal) {
+        long decimal = 0;       
+        int potencia = 0;        
+        for (int x = hexadecimal.length() - 1; x >= 0; x--) {
+            int valor = NumEquivalencia(hexadecimal.charAt(x));
+            long elevado = (long) Math.pow(16, potencia) * valor;
+            decimal += elevado;           
+            potencia++;
+        }
+        return decimal;
+    }
+
+    public static int NumEquivalencia(char number) {
+        int num = Character.getNumericValue(number);
+        switch(number){
+            case 'A':
+            num = 10;
+                break;
+
+            case 'B':
+            num = 11;
+                break;
+            
+            case 'C':
+            num = 12;
+                break; 
+        
+            case 'D':
+            num = 13;
+                break;
+            
+            case 'E':
+            num = 14;
+                break;
+            
+            case 'F':
+            num = 15;
+                break;
+        }
+        return num;
+    }
+
+    public static String EquivaleciaDecHex(int number) {
+        String num="" ;
+        switch(number){
+            case 10:
+            num = "A";
+                break;
+
+            case 11:
+            num = "B";
+                break;
+            
+            case 12:
+            num = "C";
+                break; 
+        
+            case 13:
+            num = "D";
+                break;
+            
+            case 14:
+            num = "E";
+                break;
+            
+            case 15:
+            num = "F";
+                break;
+        }
+        return num;
+    }
+
+    public String Equivalencia(int dato){
+
+        String Letra = "";
+        Letra = EquivaleciaDecHex(dato);
+        return Letra;
+    }
+    
+    public String Decimalhexadecimal(int Decimal){
+        String Hexa = "";
+        int cociente = 0;
+        int divisor = 16;
+        int residuo = 0;
+        String Resultado = "";
+        String operar = "S";
+        String hex = "";
+
+        while(operar == "S"){        
+            cociente = Decimal / divisor;
+            residuo = Decimal % divisor;
+            
+            if(residuo>9){
+                hex = Equivalencia(residuo);
+            }else{
+                hex = String.valueOf(residuo);
+            }
+                
+            if (cociente >= 16)
+            {                   
+                Resultado = Resultado + hex;                            
+                Decimal = cociente;
+            }else
+            {
+                Resultado = Resultado + hex;                                
+                operar = "N";
+            }
+        }
+        Resultado = Resultado + cociente;
+
+        //Invertir numero
+        for(int i = Resultado.length() -1;i>0 -1;i--)
+            {
+                char Digito = Resultado.charAt(i);
+                //System.out.print(Digito);
+                Hexa = Hexa + Digito;
+            }
+        return Hexa;
+    }
 }
+    
+
